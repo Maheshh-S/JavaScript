5b827abef7f803d4cb48edde830801947b426cae
Mahesh <129883017+Maheshh-S@users.noreply.github.com>
2024-06-17 11:07:09 +0000

    Datatypes02-Done!

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // it will treat all JS code as newer version of JS
+
+//readablity is imp!
+console.log(3+3);
+let age = 10;
+//string["abcd1233"]
+//int&bigint
+//bollen [true/flase]
+//null [standalone value]
+//undefined [empty]
+//symbol [Uniquw]//used more in react componets ,figma ect
+//object 
+
+
+console.log(typeof undefined );//undefined
+console.log(typeof null );//this is an object 
+
+
+
+
