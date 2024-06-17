70330a5eca58a2e6a8ed9723996754ef8a432b7c
Mahesh <129883017+Maheshh-S@users.noreply.github.com>
2024-06-17 11:06:36 +0000

    Variables01-Done!

---
+++ 01_Basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "mahesh06aa@gmail.com"
+
+var accountPassword = "12345"//not to use more or its a old type
+
+accountCity = "Jaipur"
+
+// accountId = 2//not allowed from JS
+
+accountEmail = "maahesh123@gmail.com"
+accountPassword = "543221"
+accountCity = "mysore"
+let accountState
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountCity,accountState])
