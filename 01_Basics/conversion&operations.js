dc263006de48134aa072131ccb3541af8209c4be
Mahesh <129883017+Maheshh-S@users.noreply.github.com>
2024-06-17 11:36:46 +0000

    operations03-Done!

---
+++ 01_Basics/conversion&operations.js
@@ -0,0 +1,36 @@
+//Imp 
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//basic maths operation 
+//[= - * % / ** ++]
+
+let str1 = "me & "
+let str2 = "you"
+let str3 = str1+str2
+
+console.log(str3);
+
+
+console.log("1" ,"3");
+console.log(1 + "5");
+console.log("1" + 2 +2);// string first 
+console.log(2+2 ,"2");
+
+
+console.log(+"");
+
+
+
+// let n1,n2,n3
+
+
+let gamescr = 100;
+gamescr++;
+
+console.log(gamescr);
+
+
+
